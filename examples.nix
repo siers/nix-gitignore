58448b297544b9bd5402d0d7ea039047223f3bf2
siers <wimuan@gmail.com>
2018-03-04 16:22:30 +0200

    Separate f-s-p.nix and examples.nix.

---
+++ examples.nix
@@ -0,0 +1,34 @@
+with (import <nixpkgs> {});
+with (import ./filter-source-patterns.nix { inherit lib; });
+
+let
+  # an example to get a rough feel of what the filterPattern does
+  sourcePat = builtins.filterSource (filterPattern [
+    ["^1.*/2$"  false]
+    ["^2.*/30$" true]
+    ["^2.*/.*"  false]
+  ] ./test-tree ) ./test-tree;
+
+  sourceGit = builtins.filterSource (filterPattern
+    (gitignoreToPatterns ''
+      1-simple/1
+      /1-simple/2
+      ^1-simple/3
+
+      !2-*/1?
+      !2-*/30
+      !/40
+      !50
+      2-*/*
+
+      3-*/*foo.html
+      3-*/**/bar.html
+
+      4-*/\*.html
+      4-*/o??ther.html
+      4-*/o\?ther.html
+      4-*/other.html$
+    '') ./test-tree) ./test-tree;
+
+in
+  [ sourcePat sourceGit ]
