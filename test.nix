d91b4887678dfe209582f03681f90aef5dcaea9a
siers <wimuan@gmail.com>
2018-03-05 10:48:13 +0200

    Add testing code that diffs the behavior against the real git.

---
+++ test.nix
@@ -0,0 +1,57 @@
+with (import <nixpkgs> {});
+
+let
+  nixgitignoreLocal = import ./. { inherit lib; };
+  nixgitignoreGithub = import (pkgs.fetchFromGitHub {
+    owner = "siers";
+    repo = "nix-gitignore";
+    rev = "9fe8c3a183be9b10aaa6319b5641b2f23711d5eb";
+    sha256 = "0dv0n0igx3yny2jg2bknp9l0qxvv4b032pil743aqwmyivrbbshh";
+    # nix-prefetch-git https://github.com/siers/nix-gitignore | jq -r '"\"rev\" = \"\(.rev)\";\n\"sha256\" = \"\(.sha256)\";"'
+  }) { inherit lib; };
+in
+  with nixgitignoreLocal;
+
+let
+  ignores = ''
+    1-simple/1
+    /1-simple/2
+    /1-simple/[35^$]
+
+    2-*/*
+    !2-*/1?
+    !2-*/30
+    !/40
+    !50
+
+    3-*/*foo.html
+    3-*/**/bar.html
+
+    4-*/\*.html
+    4-*/o??ther.html
+    4-*/o\?ther.html
+    4-*/other.html$
+  '';
+
+  source = ./test-tree;
+
+  sourceNix = builtins.filterSource (gitignoreFilter ignores source) source;
+
+  sourceGit = runCommand "test-tree" {} ''
+    mkdir -p $out/tmp; cd $out/tmp
+    cp -r ${source}/* .; chmod -R u+w .
+
+    cat ${builtins.toFile "nixgitignore-ignores" ignores} > .gitignore
+    ${git}/bin/git init
+    ${git}/bin/git status --porcelain --ignored | \
+      sed -n '/^!! / { s/^...//; p }' | xargs -r rm -r
+    cp -r ./* ..
+
+    cd $out; rm -rf tmp
+  '';
+
+
+in {
+  nix = sourceNix;
+  git = sourceGit;
+}
---
+++ test.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eu
+
+create-tree() { (
+    mkdir -p "$1"/{1-simple,2-negation,3-wildcards,4-escapes}
+    cd "$1"
+
+    mkdir -p 1-simple
+    touch 1-simple/{1,2,3,4,5,^,$,^$,$^}
+
+    mkdir -p 2-negation
+    touch 2-negation/{10,20,30,40,50}
+
+    mkdir -p 3-wildcards/html
+    touch 3-wildcards/{foo,bar}.html
+    touch 3-wildcards/html/{foo,bar}.html
+
+    mkdir -p 4-escapes
+    touch 4-escapes/{{*,o{,_,__,?,}ther}.html,other.html{,\$,\$\$}}
+); }
+
+list-sort() {
+    find "$1" -printf '%P\n' | sort
+}
+
+create-tree test-tree
+
+nix build '(import ./test.nix).git'
+git="$(readlink result)"; rm result
+nix="$(nix eval '(import ./test.nix).nix' --json | jq -r .)"
+
+echo "$git"
+echo "$nix"
+echo
+diff --color <(list-sort "$git") <(list-sort "$nix") || :
+
+rm -r test-tree
