6e3e31a112edaf6e6b4a030f4db433a671090535
Serhii Khoma <srghma@gmail.com>
2019-08-13 21:40:45 +0300

    refactor: cp all test dirs to localtmp dir

--- test.sh
+++ test.sh
@@ -2,15 +2,17 @@
 
 set -euo pipefail
 
-trap 'rm -rf  test-tree test-tree-recursive test-tree-gitdir' EXIT
-rm -rf  test-tree test-tree-recursive test-tree-gitdir
+trap 'rm -rf localtmp' EXIT
+rm -rf localtmp
+mkdir localtmp
 
-cp --no-preserve=all -r "$(nix-build -E '(import ./test.nix {}).sourceUnfilteredNormal')/test-tree" test-tree
-cp --no-preserve=all -r "$(nix-build -E '(import ./test.nix {}).sourceUnfilteredRecursive')/test-tree" test-tree-recursive
-cp --no-preserve=all -r "$(nix-build -E '(import ./test.nix {}).sourceUnfilteredGitdir')/test-tree" test-tree-gitdir
+cp --no-preserve=all -r "$(nix-build -E '(import ./test.nix {}).sourceUnfilteredNormal')/test-tree" localtmp/test-tree-normal
+cp --no-preserve=all -r "$(nix-build -E '(import ./test.nix {}).sourceUnfilteredRecursive')/test-tree" localtmp/test-tree-recursive
+cp --no-preserve=all -r "$(nix-build -E '(import ./test.nix {}).sourceUnfilteredGitdir')/test-tree" localtmp/test-tree-gitdir
 
+# HACK:
 # fix error
 # string '/nix/store/XXXX-test-tree' cannot refer to other paths, at ./nix-gitignore/default.nix:...
-test=$(nix-build --no-out-link -E "(import ./test.nix { sourceUnfilteredNormal' = ./test-tree; sourceUnfilteredRecursive' = ./test-tree-recursive; sourceUnfilteredGitdir' = ./test-tree-gitdir; }).success")
+test=$(nix-build --no-out-link -E "(import ./test.nix { sourceUnfilteredNormal' = ./localtmp/test-tree-normal; sourceUnfilteredRecursive' = ./localtmp/test-tree-recursive; sourceUnfilteredGitdir' = ./localtmp/test-tree-gitdir; }).success")
 
 [ -e "$test/success" ] && echo -e '\e[1;32msuccess'
